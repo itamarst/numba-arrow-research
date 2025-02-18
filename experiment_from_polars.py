e53bcb0ed5bab00712afd2aaa0ea75f573899f77
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-02-18 10:55:19 -0500

    Closer to roundtrip.

--- experiment_from_polars.py
+++ experiment_from_polars.py
@@ -16,3 +16,20 @@ def my_sum(array):
     return result
 
 print("Numba sum:", my_sum(ak_arr))
+
+@jit
+def add_one(builder, array):
+    for i in range(len(array)):
+        value = array[i]
+        if value is None:
+            builder.null()
+        else:
+            builder.integer(value + 1)
+    return builder
+
+builder = ak.ArrayBuilder()
+add_one(builder, ak_arr)
+new_ak_array = builder.snapshot()
+print("New array:", new_ak_array)
+print("New array as series:", pl.from_arrow(ak.to_arrow(new_ak_array)))
+
